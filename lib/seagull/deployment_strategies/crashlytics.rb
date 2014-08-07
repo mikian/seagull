784aa7184b5f835ca3260550f5993de29994508e
Mikko Kokkonen <mikko@owlforestry.com>
2014-08-07 12:16:26 +0800

    Added Crashlytics delivery method

---
+++ lib/seagull/deployment_strategies/crashlytics.rb
@@ -0,0 +1,33 @@
+require 'tempfile'
+require 'json'
+require 'launchy'
+
+module Seagull
+  module DeploymentStrategies
+    class Crashlytics < DeploymentStrategy
+
+      # Nothing to prepare
+      def prepare
+        unless @configuration.deploy.crashlytics_location
+          if dir = ['./', 'Pods/CrashlyticsFramework'].find{|d| ::File.directory?("#{d}/Crashlytics.framework")}
+            @configuration.deploy.crashlytics_location = "#{dir}/Crashlytics.framework"
+          end
+        end
+        raise "Please provide Crashlytics location" unless ::File.directory?(@configuration.deploy.crashlytics_location)
+      end
+
+      def deploy
+        cmd = []
+        cmd <<"#{@configuration.deploy.crashlytics_location}/submit"
+        cmd << @configuration.deploy.api_key
+        cmd << @configuration.deploy.build_secret
+        cmd << "-ipaPath #{Shellwords.escape(@configuration.ipa_full_path(@configuration.active_release_type))}"
+
+        require 'pry';binding.pry
+        system(cmd.join(' '))
+      end
+
+    private
+    end
+  end
+end
